9e7eb445e413cfc5e053fe37e0ae0355444ed5e2
Salikh Tulendybaev <salikh.tulendybaev@v.systems>
2022-11-23 15:54:01 +0800

    fix: naming

--- src/components/statcard/statcard.tsx
+++ src/components/statcard/statcard.tsx
@@ -1,14 +1,14 @@
 import { Flex, FlexProps, Text } from '@mantine/core';
-import { IconType } from 'react-icons/lib';
+
 import styles from './statcard.module.scss';
 
-interface StatCard extends FlexProps {
+interface StatCardProps extends FlexProps {
   data: string | number;
   description: string;
   icon?: JSX.Element;
 }
 
-const StatCard = (props: StatCard) => {
+const StatCard = (props: StatCardProps) => {
   return (
     <Flex className={styles.container} {...props}>
       {props.icon}
