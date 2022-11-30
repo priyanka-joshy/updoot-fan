a4e918c663c404ff8b3b77b2e22c504a03b34fd7
priyanka-joshy <98294825+priyanka-joshy@users.noreply.github.com>
2022-11-30 12:07:42 +0800

    feat: added AuthProvider and ProtectedPage to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,34 @@
 import type { AppProps } from 'next/app';
 import { AppShell } from '@mantine/core';
 
-import '../styles/globals.css';
+import '../styles/_global.scss';
 import Header from '../src/components/header';
 import Sidebar from '../src/components/sidebar';
+// amplify authentication
+import { useRouter } from 'next/router';
+import { AuthProvider } from '../src/utils/auth/authContext';
+import { ProtectedPage } from '../src/utils/auth/ProtectedPage';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
-    <AppShell
-      padding="md"
-      navbarOffsetBreakpoint="sm"
-      navbar={<Sidebar />}
-      header={<Header />}>
-      <Component {...pageProps} />
-    </AppShell>
+    <AuthProvider>
+      {router.pathname === '/' ?
+        <Component {...pageProps} />
+        :
+        <ProtectedPage>
+          <AppShell
+            padding="md"
+            navbarOffsetBreakpoint="sm"
+            navbar={<Sidebar />}
+            header={<Header />}
+          >
+            <Component {...pageProps} />
+          </AppShell>
+        </ProtectedPage>
+      }
+    </AuthProvider>
   );
 }
 
