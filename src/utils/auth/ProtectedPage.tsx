bf03050ef81e8d96766bd6180a7d5fe0c30713d0
priyanka-joshy <98294825+priyanka-joshy@users.noreply.github.com>
2022-12-05 12:11:11 +0800

    feat: access control based on user role

--- src/utils/auth/ProtectedPage.tsx
+++ src/utils/auth/ProtectedPage.tsx
@@ -2,7 +2,14 @@ import { Loader } from "@mantine/core";
 import { useRouter } from "next/router";
 import { ReactNode } from "react"
 import { useAuth } from "./authContext";
+import handleUserType from "./handleUserType";
+type UserRole = 'fan' | 'staff' | 'superAdmin';
 
+const accessRoutes: Record<UserRole, string> = {
+  fan: '/user',
+  staff: '/admin',
+  superAdmin: ''
+}
 
 export const ProtectedPage = ({ children }: { children: ReactNode }) => {
   const { user , authLoading} = useAuth();
@@ -10,12 +17,21 @@ export const ProtectedPage = ({ children }: { children: ReactNode }) => {
 
   if(!user){
     !authLoading && router.push('/')
-    return (
-      <div className="authLoader">
-        <Loader color="white" size="lg" />
-      </div>
-    );
+  } else {
+    const currentPage = router.pathname;
+    const {group, dashboardLink} = handleUserType(user);
+
+    const hasAccess = currentPage.startsWith(accessRoutes[group]);
+    !hasAccess && router.push(dashboardLink)
+
+    if(hasAccess){
+      return <>{children}</>;
+    }
   }
- 
-  return  <>{children}</>;
+
+  return (
+    <div className="authLoader">
+      <Loader color="white" size="lg" />
+    </div>
+  );
 }
