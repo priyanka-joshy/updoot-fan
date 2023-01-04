3b995d59cbb4726d108709dd93b420f0d6829152
priyanka-joshy <98294825+priyanka-joshy@users.noreply.github.com>
2023-01-04 15:13:37 +0800

    feat: util that returns currentProfilePhoto or emptyPhoto

---
+++ src/utils/storage/getProfilePhoto.ts
@@ -0,0 +1,12 @@
+import getUploadedFile from "./getUploadedFile";
+
+const getProfilePicture = async (photoURL: string) => {
+  const currentProfilePhoto = photoURL!==''? await getUploadedFile(photoURL) : undefined;
+  if(!currentProfilePhoto || currentProfilePhoto instanceof Error){
+    return '/emptyPhoto.png';
+  } else {
+    return currentProfilePhoto;
+  }
+}
+
+export default getProfilePicture;
