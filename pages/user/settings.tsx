df326d34c6f99f16b34c05b1381a86dc910ec739
priyanka-joshy <98294825+priyanka-joshy@users.noreply.github.com>
2023-01-04 17:13:11 +0800

    chore: fixed conflicting prettier

--- pages/user/settings.tsx
+++ pages/user/settings.tsx
@@ -1,8 +1,24 @@
-import { Container, Flex, Input, Loader, Modal, PasswordInput, Stack, UnstyledButton } from '@mantine/core';
+import {
+  Container,
+  Flex,
+  Input,
+  Loader,
+  Modal,
+  PasswordInput,
+  Stack,
+  UnstyledButton,
+} from '@mantine/core';
 import { GetServerSidePropsContext } from 'next';
 import { Dispatch, ReactNode, SetStateAction, useRef, useState } from 'react';
 import Button from '@components/button';
-import { BodyText, Heading1, Heading2, Heading3, Subheading2, Subheading3 } from '@components/typography';
+import {
+  BodyText,
+  Heading1,
+  Heading2,
+  Heading3,
+  Subheading2,
+  Subheading3,
+} from '@components/typography';
 import { useAuth } from 'src/utils/auth/authContext';
 import styles from 'styles/user/settings.module.scss';
 import Link from 'next/link';
@@ -14,48 +30,58 @@ import { useRouter } from 'next/router';
 import { Auth, withSSRContext } from 'aws-amplify';
 import api from 'src/utils/api';
 
-const emptyPhoto = "/emptyPhoto.png";
+const emptyPhoto = '/emptyPhoto.png';
 const maxFileSize = 10000000;
 const allowedFileTypes = ['image/png', 'image/jpeg'];
 
 interface IProps {
-  currentProfilePhoto: string
+  currentProfilePhoto: string;
 }
 interface IModalProps {
-  modalOpened: boolean, 
-  setModalOpened: Dispatch<SetStateAction<boolean>>, 
-  sticky: boolean,
-  children: ReactNode
+  modalOpened: boolean;
+  setModalOpened: Dispatch<SetStateAction<boolean>>;
+  sticky: boolean;
+  children: ReactNode;
 }
-type ModalTypes = "saving"|"saved"|"delete"|"deleting";
+type ModalTypes = 'saving' | 'saved' | 'delete' | 'deleting';
 interface ModalData {
-  icon: JSX.Element,
-  text: string,
-  btnText?: string,
-  onClick?: () => void,
-  subText?: string
+  icon: JSX.Element;
+  text: string;
+  btnText?: string;
+  onClick?: () => void;
+  subText?: string;
 }
 
-const MessageModal = ({modalOpened, setModalOpened, sticky, children}: IModalProps) => {
-  return(
+const MessageModal = ({
+  modalOpened,
+  setModalOpened,
+  sticky,
+  children,
+}: IModalProps) => {
+  return (
     <Modal
-        centered={true}
-        radius={'xl'}
-        opened={modalOpened}
-        onClose={() => setModalOpened(false)}
-        size={'30%'}
-        withCloseButton={sticky}
-        closeOnClickOutside={sticky}
-        closeOnEscape={sticky}
-      >
-        <Stack align={'center'} justify="center" mih={'34rem'} w={'75%'} ta="center" mx={'auto'}>
-          {children}
-        </Stack>
-      </Modal>
-  )
-}
+      centered={true}
+      radius={'xl'}
+      opened={modalOpened}
+      onClose={() => setModalOpened(false)}
+      size={'30%'}
+      withCloseButton={sticky}
+      closeOnClickOutside={sticky}
+      closeOnEscape={sticky}>
+      <Stack
+        align={'center'}
+        justify="center"
+        mih={'34rem'}
+        w={'75%'}
+        ta="center"
+        mx={'auto'}>
+        {children}
+      </Stack>
+    </Modal>
+  );
+};
 
-const Settings = ({currentProfilePhoto}: IProps) => {
+const Settings = ({ currentProfilePhoto }: IProps) => {
   const { user, cognitoChangePassword, cognitoLogout } = useAuth();
   const changePasswordHook = useChangePassword();
   const filePickerRef = useRef<HTMLInputElement>(null);
@@ -63,150 +89,187 @@ const Settings = ({currentProfilePhoto}: IProps) => {
 
   const [currentPhoto, setCurrentPhoto] = useState<string>(currentProfilePhoto);
   const [newPhoto, setNewPhoto] = useState<File>();
-  const [newPassword, setNewPassword] = useState<{old_password: string, new_password: string}>();
+  const [newPassword, setNewPassword] = useState<{
+    old_password: string;
+    new_password: string;
+  }>();
 
   const [passwordModal, setPasswordModal] = useState(false);
   const [msgModal, setMsgModal] = useState(false);
   const [status, setStatus] = useState<ModalTypes>();
 
-  if(!user) return null;
+  if (!user) return null;
 
-  const handleRemovePhoto = ()=> {
+  const handleRemovePhoto = () => {
     // Show empty photo
     setCurrentPhoto(emptyPhoto);
     setNewPhoto(undefined);
-  }
-  const handleNewPhoto = (e: React.ChangeEvent<HTMLInputElement>)=>{
+  };
+  const handleNewPhoto = (e: React.ChangeEvent<HTMLInputElement>) => {
     // Check file type and size
     const file = e.currentTarget.files![0];
-    if(!file) return;
+    if (!file) return;
 
-    if(file.size > maxFileSize || !allowedFileTypes.includes(file.type)){
-      console.log("File type/size not allowed."); //TODO: Show file criteria error
+    if (file.size > maxFileSize || !allowedFileTypes.includes(file.type)) {
+      console.log('File type/size not allowed.'); //TODO: Show file criteria error
       return;
     }
     setNewPhoto(e.currentTarget.files![0]);
     setCurrentPhoto(URL.createObjectURL(e.currentTarget.files![0]));
-  }
-  const handleSaveChanges = async ()=> {
-    setStatus("saving")
+  };
+  const handleSaveChanges = async () => {
+    setStatus('saving');
     setMsgModal(true);
     // Photo changed
     if (currentPhoto !== currentProfilePhoto) {
       if (currentPhoto === emptyPhoto) {
         // Remove picture in db
-        console.log("Removed profile picture");
+        console.log('Removed profile picture');
       } else {
         if (!newPhoto) return;
-        const newKey = await uploadFile(user.attributes.email, 'profile', newPhoto);
+        const newKey = await uploadFile(
+          user.attributes.email,
+          'profile',
+          newPhoto
+        );
 
         if (newKey instanceof Error) {
           console.log(newKey.message); //TODO: Show file upload error
         } else {
           // Update picture in db
-          console.log("Updated new picture: ", newKey);
+          console.log('Updated new picture: ', newKey);
         }
       }
     }
 
     // Password changed
-    if(newPassword){
+    if (newPassword) {
       const result = await cognitoChangePassword(newPassword);
-      if(result instanceof Error){
-        console.log("Error: Could not reset password. ", result.message);
-      }else{
-        console.log("Password reset successfully");
+      if (result instanceof Error) {
+        console.log('Error: Could not reset password. ', result.message);
+      } else {
+        console.log('Password reset successfully');
       }
     }
     setNewPassword(undefined);
     setNewPhoto(undefined);
-    setStatus("saved");
-  }
+    setStatus('saved');
+  };
   const handleDeleteAccount = async () => {
-    setStatus("deleting");
+    setStatus('deleting');
     try {
       await Auth.deleteUser();
       await cognitoLogout();
     } catch (error) {
       console.log('Error deleting user: ', error);
     }
-  }
+  };
 
   const profilePicture = (
     <Stack spacing={16}>
-      <img
-        src={currentPhoto}
-        className={styles.image}
-      />
+      <img src={currentPhoto} className={styles.image} />
       <input
         type="file"
         style={{ display: 'none' }}
         ref={filePickerRef}
-        onChange={(event) =>handleNewPhoto(event)}
+        onChange={(event) => handleNewPhoto(event)}
       />
-      <Button type="secondary" color="black" size="md" style={{ width: 400 }} onClick={() => handleRemovePhoto()} disabled={currentPhoto===emptyPhoto}>
+      <Button
+        type="secondary"
+        color="black"
+        size="md"
+        style={{ width: 400 }}
+        onClick={() => handleRemovePhoto()}
+        disabled={currentPhoto === emptyPhoto}>
         Remove photo
       </Button>
-      <Button type="primary" color="black" size="md" style={{ width: 400 }} onClick={() => filePickerRef.current?.click()}>
+      <Button
+        type="primary"
+        color="black"
+        size="md"
+        style={{ width: 400 }}
+        onClick={() => filePickerRef.current?.click()}>
         Upload new photo
       </Button>
-      <Subheading3 className={styles.infoText}>(JPG  or PNG, max 10MB)</Subheading3>
+      <Subheading3 className={styles.infoText}>
+        (JPG or PNG, max 10MB)
+      </Subheading3>
     </Stack>
-  )
+  );
   const resetPassword = (
     <Stack spacing={16}>
       <BodyText className={styles.infoText}>********</BodyText>
-      <Button type="primary" color="black" size="md" style={{ width: 300 }} onClick={() => setPasswordModal(true)}>
+      <Button
+        type="primary"
+        color="black"
+        size="md"
+        style={{ width: 300 }}
+        onClick={() => setPasswordModal(true)}>
         Reset Password
       </Button>
     </Stack>
-  )
+  );
 
-  const userInfo: Record<string, string|JSX.Element> = {
+  const userInfo: Record<string, string | JSX.Element> = {
     'Profile Photo': profilePicture,
-    'Username': user.attributes.name,
+    Username: user.attributes.name,
     'Phone number': user.attributes.phone_number,
-    'Email': user.attributes.email,
-    'Password': resetPassword
-  }
+    Email: user.attributes.email,
+    Password: resetPassword,
+  };
 
-  const modalData: Record<ModalTypes, ModalData>= {
+  const modalData: Record<ModalTypes, ModalData> = {
     saving: {
       icon: <Loader color="#9653f8" />,
       text: 'Saving Account',
     },
     saved: {
-      icon: <TbCircleCheck color="#6200FF" size={32}/>,
+      icon: <TbCircleCheck color="#6200FF" size={32} />,
       text: 'Account Saved',
       btnText: 'View Profile',
-      onClick: ()=>{router.push('/user/profile')}
+      onClick: () => {
+        router.push('/user/profile');
+      },
     },
     delete: {
-      icon: <TbTrashX color="#141414" size={32}/>,
+      icon: <TbTrashX color="#141414" size={32} />,
       text: 'Delete Account?',
       subText: 'Please note, after confirmation, this action cannot be undone',
       btnText: 'Confirm',
-      onClick: ()=>{handleDeleteAccount()}
+      onClick: () => {
+        handleDeleteAccount();
+      },
     },
     deleting: {
       icon: <Loader color="#9653f8" />,
       text: 'Deleting account',
-    }
-  }
+    },
+  };
   return (
     <Stack>
-      {status &&
-        <MessageModal modalOpened={msgModal} setModalOpened={setMsgModal} sticky={status==="delete"}>
+      {status && (
+        <MessageModal
+          modalOpened={msgModal}
+          setModalOpened={setMsgModal}
+          sticky={status === 'delete'}>
           {modalData[status].icon}
-          <Heading1 style={{ marginBottom: 50}}>{modalData[status].text}</Heading1>
-          <Subheading2 style={{ textAlign: "center"}}>{modalData[status].subText}</Subheading2>
-          {modalData[status].btnText && modalData[status].onClick && 
-            <Button type="primary" color="purple" style={{ width: 215 }} onClick={modalData[status].onClick}>
+          <Heading1 style={{ marginBottom: 50 }}>
+            {modalData[status].text}
+          </Heading1>
+          <Subheading2 style={{ textAlign: 'center' }}>
+            {modalData[status].subText}
+          </Subheading2>
+          {modalData[status].btnText && modalData[status].onClick && (
+            <Button
+              type="primary"
+              color="purple"
+              style={{ width: 215 }}
+              onClick={modalData[status].onClick}>
               {modalData[status].btnText}
             </Button>
-          }
+          )}
         </MessageModal>
-      }
+      )}
       <Modal
         centered={true}
         size={'80%'}
@@ -215,37 +278,58 @@ const Settings = ({currentProfilePhoto}: IProps) => {
         radius={'xl'}
         opened={passwordModal}
         onClose={() => setPasswordModal(false)}>
-        <form onSubmit={changePasswordHook.onSubmit((values) => {
-          setNewPassword({old_password: values.old_password, new_password: values.new_password});
-          setPasswordModal(false);
-        })}>
-          <Stack align={'flex-start'} spacing={36} w={'63%'} style={{ margin: '0 auto' }}>
-
+        <form
+          onSubmit={changePasswordHook.onSubmit((values) => {
+            setNewPassword({
+              old_password: values.old_password,
+              new_password: values.new_password,
+            });
+            setPasswordModal(false);
+          })}>
+          <Stack
+            align={'flex-start'}
+            spacing={36}
+            w={'63%'}
+            style={{ margin: '0 auto' }}>
             <Heading3>Reset Password</Heading3>
 
-            <PasswordInput label="Enter current password" radius={10} size="lg" {...changePasswordHook.getInputProps('old_password')} w={'100%'} />
-            <div style={{width: '100%'}}>
+            <PasswordInput
+              label="Enter current password"
+              radius={10}
+              size="lg"
+              {...changePasswordHook.getInputProps('old_password')}
+              w={'100%'}
+            />
+            <div style={{ width: '100%' }}>
               <Input.Label mb={10}>
                 <Subheading2>Enter new password</Subheading2>
               </Input.Label>
               <PasswordStrength
                 value={changePasswordHook.values.new_password}
-                onChange={(value: string) => changePasswordHook.setFieldValue('new_password', value)}
+                onChange={(value: string) =>
+                  changePasswordHook.setFieldValue('new_password', value)
+                }
               />
             </div>
-            <PasswordInput label="Confirm password" radius={10} size="lg" {...changePasswordHook.getInputProps('confirm_password')} w={'100%'} />
+            <PasswordInput
+              label="Confirm password"
+              radius={10}
+              size="lg"
+              {...changePasswordHook.getInputProps('confirm_password')}
+              w={'100%'}
+            />
 
-            <Button type="primary" color="black" style={{width: "100%"}}>
+            <Button type="primary" color="black" style={{ width: '100%' }}>
               Reset Password
             </Button>
           </Stack>
         </form>
       </Modal>
-      
+
       {/* Back Button */}
-      <Link href='/user/profile'>
+      <Link href="/user/profile">
         <UnstyledButton className={styles.backButton}>
-          <TbChevronLeft size={18}/>
+          <TbChevronLeft size={18} />
           <Subheading2>Back</Subheading2>
         </UnstyledButton>
       </Link>
@@ -254,29 +338,42 @@ const Settings = ({currentProfilePhoto}: IProps) => {
         {/* User Information */}
         <Stack spacing={48}>
           <Heading1>Settings</Heading1>
-          {Object.entries(userInfo).map(([label, item])=> (
+          {Object.entries(userInfo).map(([label, item]) => (
             <Flex align="flex-start" gap={100} key={label}>
-              <Heading2 style={{minWidth: '12.5rem'}}>{label}</Heading2>
-              {typeof (item) === "string" ?
-                <BodyText className={styles.infoText}>{item}</BodyText> 
-                :
+              <Heading2 style={{ minWidth: '12.5rem' }}>{label}</Heading2>
+              {typeof item === 'string' ? (
+                <BodyText className={styles.infoText}>{item}</BodyText>
+              ) : (
                 <>{item}</>
-              }
+              )}
             </Flex>
           ))}
         </Stack>
         {/* Save / Delete Account */}
         <Container w={'30%'} p={0} m={0}>
-          <Stack spacing={48} pos="sticky" top={"15%"}>
-            <Button type="primary" color="purple" size="lg" style={{ width: '100%' }} onClick={()=>handleSaveChanges()} disabled={currentPhoto===currentProfilePhoto && !newPassword}>
+          <Stack spacing={48} pos="sticky" top={'15%'}>
+            <Button
+              type="primary"
+              color="purple"
+              size="lg"
+              style={{ width: '100%' }}
+              onClick={() => handleSaveChanges()}
+              disabled={currentPhoto === currentProfilePhoto && !newPassword}>
               Save
             </Button>
-            <UnstyledButton className={styles.infoCard} onClick={()=>{setStatus("delete"); setMsgModal(true)}}>
+            <UnstyledButton
+              className={styles.infoCard}
+              onClick={() => {
+                setStatus('delete');
+                setMsgModal(true);
+              }}>
               <Flex align="center" gap={8}>
-                <TbTrashX size={20}/>
+                <TbTrashX size={20} />
                 <Subheading2>Delete account</Subheading2>
               </Flex>
-              <Subheading2>Please note, after confirmation, this action cannot be undone</Subheading2>
+              <Subheading2>
+                Please note, after confirmation, this action cannot be undone
+              </Subheading2>
             </UnstyledButton>
           </Stack>
         </Container>
@@ -287,7 +384,9 @@ const Settings = ({currentProfilePhoto}: IProps) => {
 
 export default Settings;
 
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
   const SSR = withSSRContext(context);
   const { name } = (await SSR.Auth.currentAuthenticatedUser()).attributes;
   // get filepath of current profile picture from db
@@ -296,7 +395,7 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
   const currentProfilePhoto = await getProfilePicture(user.profilePicture);
   return {
     props: {
-      currentProfilePhoto
-    }
-  }
+      currentProfilePhoto,
+    },
+  };
 };
--- src/components/authForm/authForm.tsx
+++ src/components/authForm/authForm.tsx
@@ -1,18 +1,13 @@
 import { useState } from 'react';
-import {
-  Stack,
-} from '@mantine/core';
+import { Stack } from '@mantine/core';
 import Button from '@components/button';
 import { Heading1, Subheading3 } from '@components/typography';
-import { 
-  AuthProcessI,
-  SignUpCredentials
-} from 'src/utils/auth/dataTypes';
+import { AuthProcessI, SignUpCredentials } from 'src/utils/auth/dataTypes';
 import SignUpForm from 'src/utils/auth/forms/signUp';
 import { Verification } from 'src/utils/auth/forms/verification';
 import { Login } from 'src/utils/auth/forms/login';
 import { ForgotPassword } from 'src/utils/auth/forms/forgotPassword';
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 
 const AuthForm = () => {
   const [formType, setFormType] = useState<AuthProcessI>('login');
@@ -21,8 +16,8 @@ const AuthForm = () => {
     username: '',
     phone_number: '',
     email: '',
-    password: ''
-  })
+    password: '',
+  });
 
   // Form titles
   const titles: Record<AuthProcessI, string> = {
@@ -35,15 +30,14 @@ const AuthForm = () => {
   // Toggle between LogIn and SignUp
   const toggleAuthButton = (
     <Button
-      size='md'
+      size="md"
       className={styles.authButton}
       type="secondary"
       color="black"
       onClick={() => {
         formType === 'signup' ? setFormType('login') : setFormType('signup');
         setAuthError(undefined);
-      }}
-    >
+      }}>
       {formType === 'signup' ? 'Log In' : 'Sign Up'}
     </Button>
   );
@@ -58,26 +52,25 @@ const AuthForm = () => {
       Forgot password?
     </Subheading3>
   );
-  
+
   const accountCreated = (
     <Button
-      size='lg'
+      size="lg"
       className={styles.authButton}
       style={{ marginTop: '40px' }}
       type="primary"
       color="purple"
-      onClick={()=>setFormType('login')}
-    >
+      onClick={() => setFormType('login')}>
       Login now
     </Button>
-  )
+  );
   // Forms for each auth process
   const authForms: Record<AuthProcessI, JSX.Element | (() => JSX.Element)> = {
     signup: (
       <>
-        <SignUpForm 
-          setAuthError={setAuthError} 
-          setFormType={setFormType} 
+        <SignUpForm
+          setAuthError={setAuthError}
+          setFormType={setFormType}
           userCred={userCred}
           setUserCred={setUserCred}
         />
@@ -85,11 +78,11 @@ const AuthForm = () => {
       </>
     ),
     confirm: (
-      <Verification 
-        type='email'
+      <Verification
+        type="email"
         userCred={userCred}
-        setAuthError={setAuthError} 
-        setFormType={setFormType} 
+        setAuthError={setAuthError}
+        setFormType={setFormType}
       />
     ),
     verified: accountCreated,
@@ -100,23 +93,24 @@ const AuthForm = () => {
         {forgotPasswordButton}
       </>
     ),
-    forgotPassword: <ForgotPassword setFormType={setFormType} setAuthError={setAuthError} />,
+    forgotPassword: (
+      <ForgotPassword setFormType={setFormType} setAuthError={setAuthError} />
+    ),
   };
   return (
     <Stack spacing={30}>
       {/* <pre>{JSON.stringify(userCred, null, 2)}</pre> */}
-      <Heading1 color='#333333' style={{ textAlign: 'center' }}>{titles[formType]}</Heading1>
-      
+      <Heading1 color="#333333" style={{ textAlign: 'center' }}>
+        {titles[formType]}
+      </Heading1>
+
       <>{authForms[formType]}</>
-      {authError && (
-        <Subheading3 color="#FF0055">
-          {authError}
-        </Subheading3>
-      )}
-      
-      <Subheading3 color='#a1a1a1' style={{ width: '87%', margin: '0 auto', textAlign: 'center' }}>
-        By signing up, you agree to our Terms of Service
-        and Privacy Policy.
+      {authError && <Subheading3 color="#FF0055">{authError}</Subheading3>}
+
+      <Subheading3
+        color="#a1a1a1"
+        style={{ width: '87%', margin: '0 auto', textAlign: 'center' }}>
+        By signing up, you agree to our Terms of Service and Privacy Policy.
       </Subheading3>
     </Stack>
   );
