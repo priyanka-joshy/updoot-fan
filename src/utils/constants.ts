0683abf3a81f1c2aaccac759e0687a97e0b84fcb
priyanka-joshy <98294825+priyanka-joshy@users.noreply.github.com>
2023-01-10 17:54:48 +0800

    chore: added constants for stardust amounts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,7 @@ export const STARDUST_CTRT_ID = 'CFFQhrW8bkiPtBd2jxrB49W6v7e85WJpsrg';
 
 export const MAIN_NET = 'https://wallet.v.systems/api';
 export const TEST_NET = 'https://veldidina-vsys.herokuapp.com';
+
+export const COMMENT_REWARD = 5;
+export const VOTE_COST = 25;
+export const PROPOSAL_COST = 200;
